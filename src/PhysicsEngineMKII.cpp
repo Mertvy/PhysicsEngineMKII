2da50d403643c81e9b6aa77b27dc147c9e556327
CapybaraLover45 <alexandersp332@gmail.com>
2024-07-19 14:28:30 -0400

    added glad instead of glew. have test window in main

--- src/PhysicsEngineMKII.cpp
+++ src/PhysicsEngineMKII.cpp
@@ -1,14 +1,15 @@
 ﻿// PhysicsEngineMKII.cpp : Defines the entry point for the application.
 //
 
-#include <GL/glew.h>
+#include <glad/glad.h>
+#include <KHR/khrplatform.h>
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 #include "imgui/imgui.h"
 #include "imgui/backends/imgui_impl_glfw.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
 #include <iostream>
-
+#include <string>
 
 
 static void glfw_error_callback(int error, const char* description);
@@ -16,43 +17,95 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 
+static unsigned int CompileShader(unsigned int type, const std::string& source);
+static unsigned int CreateShader(const std::string& vert, const std::string& frag);
+
+
 int main()
 {
+	///////////// set up shit for glfw/glad
 	glfwSetErrorCallback(glfw_error_callback);
 	if (!glfwInit()) return 1;
-	const char* glsl_version = "#version 130";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	GLFWwindow* window = glfwCreateWindow(600, 800, "PhysicsEngineMKII", nullptr, nullptr);
+	const char* glsl_version = "#version 330";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(720, 480, "PhysicsEngineMKII", nullptr, nullptr);
 	if (window == nullptr) return 1;
 	glfwSetKeyCallback(window, key_callback);
 	glfwMakeContextCurrent(window);
-
 	glfwSwapInterval(1); // Enable vsync	
-
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+	glViewport(0, 0, 720, 480);
+	//imgui
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-
 	ImGui::StyleColorsDark();
-
 	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init(glsl_version);
+	//////////////// 
+
+
+
+	std::string vs = R"(
+	#version 330 core
+
+layout(location = 0)in vec2 vPos; 
+
+void main() {
+    gl_Position = vec4(vPos, 0.0,1.0);    
+}
+	)";
+
+	std::string fs = R"(
+	#version 330 core
+
+out vec4 color; 
+
+void main() {
+	color = vec4(0.0,1.0,0.5,1.0);
+})";
+
+	static unsigned int test = CreateShader(vs, fs);
+
+
+	float vertices[] = {
+		-0.5f, -0.5f,
+		0.5f, -0.5f,
+		0.0f, 0.5f
+	};
+	unsigned int vbo, vao;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 
 	while (!glfwWindowShouldClose(window)) {
-		// Keep running
-		glfwPollEvents();
-		// Start the Dear ImGui frame
+		glClearColor(0, 0.0, 0.5, 0.0);
+		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-
+	
+	
 		{
 			ImGui::Begin("Test");
 			ImGui::End();
 		}
+		ImGui::ShowDemoWindow();
+		
+		glUseProgram(test);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
 
 
 
@@ -63,16 +116,15 @@ int main()
 		int display_w, display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
 		glViewport(0, 0, display_w, display_h);
-		glClearColor(0,0.5,1,1);
-		glClear(GL_COLOR_BUFFER_BIT);
+		
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
 
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
-
 	return 0;
 }
 
@@ -89,3 +141,42 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile: " << type << ": " << message << std::endl;
+		glDeleteShader(id);
+		return -1;
+	}
+
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vert, const std::string& frag) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vert);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, frag);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	return program;
+}
+
+
